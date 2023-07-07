535fdacad0a5a5b27c610a8e8166649bac84ee01
Bahareh <izad@bu.edu>
2023-07-07 14:20:12 -0400

    added postorder traversal

---
+++ Twopointer/Binary search/Linked List/Trees/145. Binary Tree Postorder Traversal.py
@@ -0,0 +1,8 @@
+
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+
+        if root is None:
+            return []
+        else:
+            return self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val]
